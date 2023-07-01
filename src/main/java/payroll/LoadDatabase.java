d11b9b8bafe8a1f7c1fde17adf518155034fffbb
Ivan Bakula <wamreu@gmail.com>
2023-07-01 17:31:36 +0200

    FIX đubre

--- src/main/java/payroll/LoadDatabase.java
+++ src/main/java/payroll/LoadDatabase.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-mport org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 class LoadDatabase {
