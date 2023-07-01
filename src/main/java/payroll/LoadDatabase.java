a7f1f5773881db69cfd2d99b28845026332956f8
Ivan Bakula <wamreu@gmail.com>
2023-07-01 13:47:03 +0200

    Break CI

--- src/main/java/payroll/LoadDatabase.java
+++ src/main/java/payroll/LoadDatabase.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+mport org.springframework.context.annotation.Configuration;
 
 @Configuration
 class LoadDatabase {
